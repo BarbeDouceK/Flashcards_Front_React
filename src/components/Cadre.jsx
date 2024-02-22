ec802c1a26871ef6a4ab943435b02bbf140b0449
BarbeDouce <baptiste.louerat44@gmail.com>
2024-02-22 14:26:14 +0100

    Ceci va tout casser

--- src/components/Cadre.jsx
+++ src/components/Cadre.jsx
@@ -1,7 +1,16 @@
 function Cadre(){
     return(
         <>
-
+            <div className="p-8">
+                <div className="px-6">
+                    <div className="center">
+                        <a href="/"><h1>Application FlashCards</h1></a>
+                    </div>
+                    <div>
+                        <NavBar/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
