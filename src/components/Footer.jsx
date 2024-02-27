d04c7da4bb5570e3815c961a2d7e7902fe0ae419
Baptiste <baptiste.louerat44@gmail.com>
2024-02-27 12:10:26 +0100

    Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <>
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by Chuche Romain - Louërat Baptiste</p>
+                </aside>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
