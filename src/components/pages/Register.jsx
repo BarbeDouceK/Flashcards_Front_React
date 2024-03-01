61167cf397983d437cb47e27b711074e41369eff
Sukushi <romain.chuche@gmail.com>
2024-03-01 10:36:10 +0100

    Yannis register ok

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,11 +1,42 @@
-import { useEffect, useState } from "react";
-import ApiService from "../../service/ApiService";
+import {httpClient} from "../../service/http.client.js";
+import {useNavigate} from "react-router-dom";
 
 export const Register = () => {
-
+    const navigate = useNavigate()
     return (
         <>
-        
-        </>
-    )
+            <div
+                className="w-1/2 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <form className="space-y-6 flex flex-col" onSubmit={(e) => {e.preventDefault()
+                    httpClient.api.post("auth/register",Object.fromEntries(new FormData(e.target).entries())).then(() => navigate("/auth"))
+                }}>
+                    <h5 className="text-xl font-medium text-gray-900 dark:text-white">Register to our platform</h5>
+                    <div>
+                        <label htmlFor="name"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
+                            name</label>
+                        <input type="text" id="name" name="name"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="John Doe" required/>
+                    </div>
+                    <div className="">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email
+                            address</label>
+                        <input type="email" id="email" name="email"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               placeholder="john.doe@company.com" required/>
+                    </div>
+                    <div className="">
+                        <label htmlFor="password"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" id="password" name="password"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               placeholder="•••••••••" required/>
+                    </div>
+                    <button type="submit"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+                    </button>
+                </form>
+            </div>
+        </>)
 }
